56a687a906a2a540f21e1894831e725a4c34f651
Jesse Franceschini <j.franceschini@gmail.com>
2015-01-21 22:07:42 -0800

    Ian changes.

--- main.py
+++ main.py
@@ -1,70 +1,55 @@
+## Ifeanyi Ibe
+## Lab 3: Section Times
+##
+
+
+section01 = "8:00 - 8:50am, Daily"
+section02 = "9:00 - 9:50am, Daily"
+section03 = "10:00 - 10:50am, Daily"
+section04 = "11:00 - 11:50am, Daily"
+section05 = "12:00 - 1:05pm, MTWTh"
+section06 = "1:15 - 2:20pm, MTWTh"
+section08 = "6:00 - 8:20pm, MW"
+section09 = "6:00 - 8:20pm, TTh"
+
+
 # gather information from the user
 dept = input('Enter the department code: ')
 course = input('Enter the course number: ')
 section = input('Enter the section number: ')
 
+# Initialize some values
 error = False
+output = ""
 
-# Section Number Tables
-section_nums = {
-    '01': {
-        'times': '8:00 - 8:50 am',
-        'days': 'Daily'
-    },
-    '02': {
-        'times': '9:00 - 9:50 am',
-        'days': 'Daily'
-    },
-    '03': {
-        'times': '10:00 - 10:50 am',
-        'days': 'Daily'
-    },
-    '04': {
-        'times': '11:00 - 11:50 am',
-        'days': 'Daily'
-    },
-    '05': {
-        'times': '12:00 - 1:05 pm',
-        'days': 'MTWTh'
-    },
-    '06': {
-        'times': '1:15 - 2:20 pm',
-        'days': 'MTWTh'
-    },
-    '08': {
-        'times': '6:00 - 8:20 pm',
-        'days': 'MW'
-    },
-    '09': {
-        'times': '6:00 - 8:20 pm',
-        'days': 'TTh'
-    }
-}
+def prepare(selection):
+    return 'Your course {} {}-{} meets {}'.format(
+        dept,
+        course,
+        section,
+        selection)
 
-def lookup(code):
-    try:
-        return section_nums[code]
-    except KeyError:
-        return 'unknown'
+if section == str("01"):
+    output = prepare(section01)
+elif section == str("02"):
+    output = prepare(section02)
+elif section == str("03"):
+    output = prepare(section03)
+elif section == str("04"):
+    output = prepare(section04)
+elif section == str("05"):
+    output = prepare(section05)
+elif section == str("06"):
+    output = prepare(section06)
+elif section == str("08"):
+    output = prepare(section08)
+elif section == str("09"):
+    output = prepare(section09)
+else :
+    error = True
+    output = "ERROR: Unrecognized section number."
 
-def output(meeting):
-    global error
-    try:
-        return 'Your course {} {}-{} meets {}, {}'.format(
-            dept,
-            course,
-            section,
-            meeting.get('times'),
-            meeting.get('days'))
-    except AttributeError:
-        error = True;
-        return 'ERROR: Unrecognized section number.'
-
-# determine when the class meets: the error state is triggered here
-meeting = lookup(section)
-printout = output(meeting)
-
-
+# do error checks only if the section number is valid
 if not error:
     if not dept.isupper():
         print('WARNING: departement code is not upper-case.')
@@ -73,10 +58,8 @@ if not error:
     if len(course) != 3:
         print('WARNING: course number is not 3-digits.')
 
-    try:
-        val = int(course)
-    except ValueError:
+    if not course.isnumeric():
         print('WARNING: course number is not numeric.')
 
 # output the information to the user
-print(printout)
+print(output)
